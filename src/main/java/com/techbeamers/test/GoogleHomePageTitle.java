d9aeea9ecc6e01181e16da71fed62dca3b7e2989
fadibarcelona <41168516+fadibarcelona@users.noreply.github.com>
2019-09-07 08:12:12 +0500

    Update GoogleHomePageTitle.java

--- src/main/java/com/techbeamers/test/GoogleHomePageTitle.java
+++ src/main/java/com/techbeamers/test/GoogleHomePageTitle.java
@@ -24,7 +24,7 @@ public class GoogleHomePageTitle {
 	public void verifyGooglePageTittle() {
 		driver.navigate().to(URL);
 		String getTitle = driver.getTitle();
-		Assert.assertEquals(getTitle, "Fawad");
+		Assert.assertEquals(getTitle, "Google");
 	}
 	
 	@AfterClass
